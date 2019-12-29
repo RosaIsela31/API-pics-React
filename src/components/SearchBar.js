6f58421cee67972ea9af4c7ec6aeed42eaa6bcfc
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-12-29 11:07:48 -0600

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render(){
+    return(
+      <div>
+        <input type='text' placeholder='what'/>
+      </div>
+    );
+  };
+};
+
+export default SearchBar;
